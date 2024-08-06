e92d2b847bf9ce62de38e7547d67f4b183a09d0c
MatFin <45946113+MatteoFina@users.noreply.github.com>
2024-08-07 01:09:34 +0200

    push

--- docs/render-docs.R
+++ docs/render-docs.R
@@ -6,10 +6,10 @@ library(rmarkdown)
 library(xaringan)
 
 # non-xaringan -----------------------------------------------------------------
-# rmds <- dir_info(recurse = 3, glob = "docs/*.Rmd") %>%
-#   filter(!str_detect(path, "slides")) %>%
-#   pull(path)
-# walk(rmds, render)
+rmds <- dir_info(recurse = 3, glob = "docs/*.Rmd") %>%
+  filter(!str_detect(path, "slides")) %>%
+  pull(path)
+walk(rmds, render)
 
 # xaringan ---------------------------------------------------------------------
 
@@ -17,11 +17,11 @@ xaringans <- dir_info(recurse = 3, glob = "docs/*.Rmd") %>%
   filter(str_detect(path, "slides")) %>%
   filter(!str_detect(path, "setup")) %>%
   # set filter to match the changed files
-  #  filter(str_detect(path,
+    filter(str_detect(path,
   #   # comment/un-comment here to update the slide(s):
-  #   "01-hello-world.Rmd"
-  #   #"01-welcome.Rmd"
-  #   # "02-toolkit",
+  #   "01-hello-world.Rmd",
+     "01-welcome.Rmd"
+  #    "02-toolkit",
   #   # "01-data-viz",
   #   # "02-ggplot2",
   #   # "04-viz-cat",
@@ -32,7 +32,7 @@ xaringans <- dir_info(recurse = 3, glob = "docs/*.Rmd") %>%
   #   # "10-data-types",
   #   # "12-data-import",
   #   # "02-fitting-interpreting-models"
-  # )) %>%
+   )) %>%
   filter(!str_detect(path, "22")) %>%
   pull(path)
 
